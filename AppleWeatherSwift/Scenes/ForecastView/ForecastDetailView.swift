a3a6773c7db65e215642c064705c6fe2fef635ef
Filip Štěpánek <>
2024-05-24 08:35:15 +0200

    App update - View ForecastDetailView and ForecastDetailViewNow were merged, because there had same UI but data were different. Now is only ForecastDetailView with data logic for both of data.

--- AppleWeatherSwift/Scenes/ForecastView/ForecastDetailView.swift
+++ AppleWeatherSwift/Scenes/ForecastView/ForecastDetailView.swift
@@ -7,38 +7,76 @@
 
 import SwiftUI
 
-struct ForecastDetailView: View {
+protocol WeatherData {
+    var temperature: Double { get }
+    var icon: String { get }
+    var weatherInfo: String { get }
+    var dateInfo: String { get }
+}
+
+extension ForecastResponse.ListResponse: WeatherData {
+    var temperature: Double {
+        return main.temp
+    }
     
-    let weather: ForecastResponse.ListResponse
+    var icon: String {
+        return weather.first?.icon ?? ""
+    }
+    
+    var weatherInfo: String {
+        return WeatherManagerExtension().getWeatherInfoFromForecastIcon(icon: icon)
+    }
+    
+    var dateInfo: String {
+        return Date.formatUnixTimestampInGMT(self.date)
+    }
+}
+
+extension CurrentResponse: WeatherData {
+    var temperature: Double {
+        return main.temp
+    }
+    
+    var icon: String {
+        return weather.first?.icon ?? ""
+    }
+    
+    var weatherInfo: String {
+        return WeatherManagerExtension().getWeatherInfoFromForecastIcon(icon: icon)
+    }
+    
+    var dateInfo: String {
+        return "Now"
+    }
+}
+
+struct ForecastDetailView: View {
+    let weatherData: WeatherData
     
     var body: some View {
-        
         let temperatureWithUnits = "\(temperatureUnitSymbol())"
         
-        HStack(
-            spacing: 17
-            
-        ) {
+        HStack(spacing: 17) {
             ZStack {
-                
                 Circle()
                     .frame(maxWidth: 48, maxHeight: 48)
                     .cornerRadius(48)
                     .foregroundColor(.iconBase)
-                Image(WeatherManagerExtension().getImageNameFromForecastIcon(icon: weather.weather.first?.icon ?? ""))
+                Image(WeatherManagerExtension().getImageNameFromForecastIcon(icon: weatherData.icon))
                     .imageSize()
-                
             }
             .padding(.leading, 16)
             
-            VStack(
-                alignment: .leading
+            VStack(alignment: .leading) {
+                if let listResponse = weatherData as? ForecastResponse.ListResponse {
+                    Text(listResponse.dateInfo)
+                        .modifier(ContentMediumModifier())
+                } else if let currentResponse = weatherData as? CurrentResponse {
+                    Text(currentResponse.dateInfo)
+                        .modifier(ContentMediumModifier())
+                }
                 
-            ) {
-                Text(Date.formatUnixTimestampInGMT(weather.date))
-                    .modifier(ContentMediumModifier())
-                
-                Text(WeatherManagerExtension().getWeatherInfoFromForecastIcon(icon: weather.weather.first?.icon ?? ""))
+                Text(weatherData.weatherInfo)
                     .modifier(ContentSmallInfoModifier())
             }
             
@@ -46,27 +84,45 @@ struct ForecastDetailView: View {
             
             Text(temperatureWithUnits)
                 .modifier(HeadlineThreeModifier())
-            
                 .padding()
         }
         .frame(maxWidth: .infinity)
         .background(.row)
         .cornerRadius(16)
     }
+    
     func temperatureUnitSymbol() -> String {
         let measurementFormatter = MeasurementFormatter()
         measurementFormatter.numberFormatter.maximumFractionDigits = 0
         
-        let temperature = Measurement(value: weather.main.temp, unit: UnitTemperature.celsius)
+        let temperature = Measurement(value: weatherData.temperature, unit: UnitTemperature.celsius)
         return measurementFormatter.string(from: temperature)
     }
 }
 
 #if DEBUG
-#Preview {
-        let mockListResponse = ForecastResponse.ListResponse(date: 1702749600, main: ForecastResponse.MainResponseForecast(temp: 0), weather: [])
-        let mockForecastResponse = ForecastResponse(city: ForecastResponse.CoordinatesResp(coord: ForecastResponse.Coordinates(lon: 0, lat: 0)), list: [mockListResponse])
+struct ForecastDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        let mockForecastResponse = ForecastResponse.ListResponse(
+            date: 1702749600,
+            main: ForecastResponse.MainResponseForecast(temp: 30),
+            weather: [ForecastResponse.WeatherResponseForecast(icon: "01d")]
+        )
         
-        return ForecastDetailView(weather: mockForecastResponse.list.first ?? mockListResponse)
+        let mockCurrentResponse = CurrentResponse(
+            coord: CurrentResponse.CoordinatesResponse(lon: 0, lat: 0),
+            weather: [CurrentResponse.WeatherResponse(id: 800, main: "Clear", description: "clear sky", icon: "01d")],
+            main: CurrentResponse.MainResponse(temp: 25, pressure: 1013, humidity: 60),
+            name: "Sample City",
+            wind: CurrentResponse.WindResponse(speed: 5.0, deg: 180),
+            sys: CurrentResponse.CountryName(country: "Sample Country"),
+            rain: nil
+        )
+
+        return Group {
+            ForecastDetailView(weatherData: mockForecastResponse)
+            ForecastDetailView(weatherData: mockCurrentResponse)
+        }
     }
+}
 #endif
