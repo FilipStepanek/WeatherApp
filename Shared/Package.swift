66d0068dccf83341a8ef6631ebb7056bab8790b3
Filip Štěpánek <>
2024-06-19 13:04:57 +0200

    Shared files added to Package.

--- Shared/Package.swift
+++ Shared/Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Shared",
+    platforms: [
+        .iOS(.v16),
+        .watchOS(.v9),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
